bfacd50db300dea0e0bd0e57ba96adedac03d3fa
Crstt <matteo598.catalano@gmail.com>
2023-06-16 14:14:23 -0400

    updated oregon_trail file

---
+++ player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self, character:str, money:int, food:int, huntAdjust:float, buyHudjust:float, distNext:int):
+        self.character = character
+        self.money = money
+        self.food = food
+        self.huntAdjust = huntAdjust
+        self.buyHudjust = buyHudjust
+        self.distNext = distNext
+
+    def travel():
+        #call scenario
+        #if successful update milage
+        pass
+
+    def hunt():
+        #call scenario hunt
+        pass
