4dc8539cb2381c9ed932274a7ec27e47e9a1fca1
Crstt <matteo598.catalano@gmail.com>
2023-06-20 17:56:55 -0400

    add first implementation of GUI. Add sample imgs

--- oregon_trail.py
+++ oregon_trail.py
@@ -1,6 +1,10 @@
 import scenarioManager
 import player
 import checkpointManager
+import gui
 
-
+# create a gui instance
+root = gui.StartGui()
+#starts the gui main loop
+root.mainloop()
 #implement instance of player
