d8f6edb49d2c0c2a96d93039324dfcc8b59d4933
13179 <christopherhochgesang@gmail.com>
2023-06-21 19:43:35 -0400

    -Fills out checkpoint manager -Modifies 2 checkpoint types to include a name, removes some parameters -includes a few tests for the checkpoint manager -initialization of some checkpointers

--- checkpoints/town.py
+++ checkpoints/town.py
@@ -1,9 +1,10 @@
 class TownCheckpoint:
-    def __init__(self, isRiver=False, isTown=False, distNextCP=0):
-        self.isRiver = isRiver
-        self.isTown = isTown
+    def __init__(self, distNextCP=0,name=""):
+        self.isRiver = False
+        self.isTown = True
         self.distNextCP = distNextCP
-    
+        self.name=name
+        
     def updateState(self):
         print("Updating checkpoint state...")
         # Implement the logic to update the state of the checkpoint here
