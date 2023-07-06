af7756f9601f36ddfb594b3c7a63f6d1a60688a8
Nathan Able <nable@ivytech.edu>
2023-07-06 14:09:20 -0400

    Player Update
    Updated player with character creation function. Function includes notes on connection points to make it operable with the GUI. See also the test file to see none GUI functionality

--- player.py
+++ player.py
@@ -15,59 +15,42 @@ class Player:
         self.buyAdjust = buyAudjust
         self.distNext = distNext
 
-    def createPlayer():
-        filename = "characters.txt"
-        characters = {}
-        with open(filename, 'r') as csvfile:
-            for row in csv.reader(csvfile):
-                characters[row[0]] = Player(*row[1:])
-        return characters
+def selectCharacter():
+    filename = "characters.txt"
+    choices = {}
+    with open(filename, 'r') as csvfile:
+        for row in csv.reader(csvfile):
+            choices[row[0]] = Player(*row[1:])
+    
+    cKey = [*choices]
+    c=0
+    i= 0
+    button2Label = "See Next Option"
+    while c == 0:
+        loopBoundry = len(choices)
+        if i == loopBoundry:
+            i=0
+        #create character description for display
+        hunt = str(float(choices[cKey[i]].huntAdjust) * 100)
+        buy = str(float(choices[cKey[i]].buyAdjust) * 100)
+        discriptionLabel = "The" + choices[cKey[i]].character + " starts with $" + choices[cKey[i]].money + ", and a " + hunt + " percent change to hunting as well as " + buy  +" percent change to buying costs."
+        button1Label = "Choose " + choices[cKey[i]].character
+        """
+        This will need to be editied to configure to the GUI button clicks beyond this point. This is essentially a sample of what the code should sortof look like
+        The input and if statements are for testing purposes only
+        button1.click()
+            c=i
+        button2.click()
+            i+=1
+        """
+        print(discriptionLabel)
+        print(button1Label)
+        print(button2Label)
 
-    def selectCharacter():
-        filename = "characters.txt"
-        choices = {}
-        with open(filename, 'r') as csvfile:
-            for row in csv.reader(csvfile):
-                choices[row[0]] = Player(*row[1:])
-        
-        cKey = [*choices]
-        c=0
-        i= 0
-        button2Label = "See Next Option"
-        while c == 0:
-            loopBoundry = len(choices)
-            if i == loopBoundry:
-                i=0
-            #create character description for display
-            hunt = str(float(choices[cKey[i]].huntAdjust) * 100)
-            buy = str(float(choices[cKey[i]].buyAdjust) * 100)
-            discriptionLabel = "The" + choices[cKey[i]].character + " starts with $" + choices[cKey[i]].money + ", and a " + hunt + " percent change to hunting as well as " + buy  +" percent change to buying costs."
-            button1Label = "Choose " + choices[cKey[i]].character
-            """
-            This will need to be editied to configure to the GUI button clicks beyond this point. This is essentially a sample of what the code should sortof look like
-            The input and if statements are for testing purposes only
-            button1.click()
-                c=i
-            button2.click()
-                i+=1
-            """
-            print(discriptionLabel)
-            print(button1Label)
-            print(button2Label)
-
-            selection = input("what button do you click? b1 - b2")
-            if selection == "b1":
-                c=i
-                return (choices[cKey[c]].character, choices[cKey[c]].money, choices[cKey[c]].food, choices[cKey[c]].huntAdjust, choices[cKey[c]].buyAdjust, choices[cKey[c]].distNext)
-            else: i += 1
+        selection = input("what button do you click? b1 - b2  ")
+        if selection == "b1":
+            c=i
+            return choices[cKey[c]].character, choices[cKey[c]].money, choices[cKey[c]].food, choices[cKey[c]].huntAdjust, choices[cKey[c]].buyAdjust, choices[cKey[c]].distNext
+        else: i += 1
 
             
-
-player1 = Player(Player.selectCharacter())
-
-print(player1.character)
-print(player1.money)
-print(player1.food)
-print(player1.huntAdjust)
-print(player1.buyAdjust)
-print(player1.distNext)
